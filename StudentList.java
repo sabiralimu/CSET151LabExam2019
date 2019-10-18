938855272e4f60a3bc38d06bb65bcbfebd4d53d1
Sabira Limu <sabiralimu@gmail.com>
2019-10-18 17:34:12 +0600

    STEP #1.Update code style for better consistency git status
    git commit -m

--- StudentList.java
+++ StudentList.java
@@ -2,22 +2,11 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 
 //		Check arguments
-		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");
-		}
-		else if(args[0].equals("r")) 
+		if(args[0].equals("a")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
@@ -25,17 +14,35 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			
+			String i[] = r.split(",");			
+			for(String j : i) { System.out.println(j);
+                               }
+			            } 
+                 catch (Exception e){} 
+			System.out.println("Data Loaded.");
+		}
+		else if(args[0].equals("r")) 
+		{
+			System.out.println("Loading data ...");			
+			try 
+			{
+			BufferedReader s = new BufferedReader(
+					new InputStreamReader(
+							new FileInputStream("students.txt")));  
+			String r = s.readLine();
 			String i[] = r.split(",");	
 			Random x = new Random();
 				int y = x.nextInt(i.length);
 					System.out.println(i[y]);
-			} catch (Exception e){} 
+			} 
+			catch (Exception e){} 
 			System.out.println("Data Loaded.");			
 		}
-		else if(args[0].contains("+")){
+		else if(args[0].contains("+"))
+		{
 			System.out.println("Loading data ...");			
-			try {
+			try
+			 {
 			BufferedWriter s = new BufferedWriter(
 					new FileWriter("students.txt", true));
 			String t = args[0].substring(1);
@@ -60,7 +67,8 @@ public class StudentList {
 			String i[] = r.split(",");	
 			boolean done = false;
 			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
+			for(int idx = 0; idx<i.length && !done; idx++)
+			 {
 				if(i[idx].equals(t)) {
 					System.out.println("We found it!");
 						done=true;
@@ -83,7 +91,8 @@ public class StudentList {
 			for(char c:a) {
 				if(c ==' ') 
 				{
-					if (!in_word) {	count++; in_word =true;	}
+					if (!in_word) {	count++; in_word =true;
+	}
 					else { in_word=false;}			
 				}
 			}
