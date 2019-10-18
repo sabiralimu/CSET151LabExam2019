926589255b42ee312ad63aebb34fd12afc5d500e
Sabira Limu <sabiralimu@gmail.com>
2019-10-18 18:29:25 +0600

    change variable name

--- StudentList.java
+++ StudentList.java
@@ -16,12 +16,12 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j);
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");			
+			for(String word: reader) { System.out.println(j);
                                }
 			            } 
                  catch (Exception e){} 
@@ -32,14 +32,14 @@ public class StudentList {
 			System.out.println("Loading data ...");			
 			try 
 			{
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt")));  
-			String r = s.readLine();
-			String i[] = r.split(",");	
-			Random x = new Random();
-				int y = x.nextInt(i.length);
-					System.out.println(i[y]);
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");	
+			Random rand = new Random();
+				int randomIndex = rand.nextInt(words.length);
+					System.out.println(words[y]);
 			} 
 			catch (Exception e){} 
 			System.out.println("Data Loaded.");			
@@ -49,15 +49,15 @@ public class StudentList {
 			System.out.println("Loading data ...");			
 			try
 			 {
-			BufferedWriter s = new BufferedWriter(
+			BufferedWriter fileStream = new BufferedWriter(
 					new FileWriter("students.txt", true));
-			String t = args[0].substring(1);
-	        Date d = new Date();
+			String argValue = args[0].substring(1);
+	        Date fileContents = new Date();
 	        String df = "dd/mm/yyyy-hh:mm:ss a";
 	        DateFormat dateFormat = new SimpleDateFormat(df);
-	        String fd= dateFormat.format(d);
-			s.write(", "+t+"\nList last updated on "+fd);
-			s.close();
+	        String fd= dateFormat.format(fileContents);
+			fileStream.write(", "+t+"\nList last updated on "+fd);
+			fileStream.close();
 			} catch (Exception e){}
 							
 			System.out.println("Data Loaded.");	
@@ -66,16 +66,16 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");	
 			boolean done = false;
-			String t = args[0].substring(1);
+			String argValue = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++)
 			 {
-				if(i[idx].equals(t)) {
+				if(words[idx].equals(argValue)) {
 					System.out.println("We found it!");
 						done=true;
 				}
@@ -83,18 +83,19 @@ public class StudentList {
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
+
 		else if(args[0].contains("c")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String D = s.readLine();
-			char a[] = D.toCharArray();			
+			String fileContents = fileStream.readLine();
+			char characters[] = fileContents.toCharArray();			
 			boolean in_word = false;
 			int count=0;
-			for(char c:a) {
+			for(char c:characters) {
 				if(c ==' ') 
 				{
 					if (!in_word) {	count++; in_word =true;
@@ -102,7 +103,7 @@ public class StudentList {
 					else { in_word=false;}			
 				}
 			}
-			System.out.println(count +" word(s) found " + a.length);
+			System.out.println(count +" word(s) found " + characters.length);
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
